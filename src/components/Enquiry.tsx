2804916ed5a5fdbbaa9f2c943868b7d3e7d22874
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-01-23 13:39:58 -0800

    need to do a few stuffs, done with the landing page tho, just responsiveness and and few design stuff

---
+++ src/components/Enquiry.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+const Enquiry = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center px-52 mb-28">
+      <p className="text-center text-[2rem] font-bold">
+        If you have any question or need assistance,
+      </p>
+      <p className="text-center text-[2rem] font-bold">
+        please do not hesitate to reach out to us
+      </p>
+      <form onSubmit={handleSubmit} className="max-w-md w-[400px] mx-auto mt-8">
+        <div className="mb-4">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            placeholder="Subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+            
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+            className="mt-1 p-2 w-full border rounded-md"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-[#D8A331] text-white px-4 py-2 rounded-[30px] w-full mt-4"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Enquiry;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#29176B]">
+      <div className="bg-[#29176B] h-[200px] w-full flex flex-col justify-center items-center">
+        <div className="flex justify-evenly text-white flex justify-between items-center">
+          <div className="w-[100px] border border-[#888]">
+            <p>twitter</p>
+          </div>
+          <div className="w-[100px] border border-[#888]">
+            <p>instagram</p>
+          </div>
+          <div className="w-[100px] border border-[#888]">
+            <p>linkedin</p>
+          </div>
+          <div className="w-[100px] border border-[#888]">
+            <p>facebook</p>
+          </div>
+        </div>
+      </div>
+      <p className="text-center text-[12px] text-[#fff]">© 2023 Chumskiisama. All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
