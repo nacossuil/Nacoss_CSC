23b7b22d38533612c2ddb57c5f4e9e28336fb757
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-01-24 16:40:56 -0800

    90% done, just reponsiveness to deal with

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,35 @@
 import React from "react";
-import about from "../assets/about.png"
+import about from "../assets/about.png";
+import star from "../assets/star3.png";
+import direction from "../assets/Direction2.png";
+import infinity from "../assets/Infinity.png"
 
 const About = () => {
   return (
-    <div className="flex justify-evenly items-center py-48">
-      <div className="w-[40%]">
-        <p>ABOUT US</p>
-        <p>Most Organised Student Body in Unilorin</p>
-        <p>
-          Nigeria Association of Computing Students (NACOS) is the umbrella body
-          for students studying Computer Science, Computer Engineering,
-          Information Systems, Cyber Security, Software Engineering,
-          Telecommunications Engineering, and all IT-related courses in Nigeria
-          and have been in existence for over two decades.
-        </p>
+    <div>
+      <img src={star} alt="" className="relative top-[22rem] left-[32rem] w-[2rem] h-[2rem]" />
+      <div className="flex flex-wrap justify-evenly items-center py-24">
+        <div className="w-[33%]">
+          <p className="text-[#C49C45] font-bold text-[20px] leading-[28px] mb-6">
+            ABOUT US
+          </p>
+          <p className="font-bold text-[36px] leading-[48px]">
+            Most Organised Student Body in Unilorin
+          </p>
+          <p className="text-[1rem] leading-[28px] mt-6">
+            Nigeria Association of Computing Students (NACOS) is the umbrella
+            body for students studying Computer Science, Computer Engineering,
+            Information Systems, Cyber Security, Software Engineering,
+            Telecommunications Engineering, and all IT-related courses in
+            Nigeria and have been in existence for over two decades.
+          </p>
+          <img src={infinity} alt="" className="w-[2rem] h-[1rem] relative top-6"/>
+        </div>
+        <div>
+          <img src={about} />
+        </div>
       </div>
-      <div>
-        <img src={about} />
-      </div>
+      <img src={direction} alt="" className="relative left-[56rem] bottom-4 w-[2rem] h-[1rem]" />
     </div>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import hero from "../assets/hero.png"
+import grid from "../assets/Grid.png"
+import star1 from "../assets/star1.png"
+import star2 from "../assets/star2.png"
+import infinity from "../assets/Infinity.png"
+import direction from "../assets/Direction.png"
 
 const Hero = () => {
   return (
-    <div className='container mx-auto flex flex-col justify-center items-center pt-16'>
+    <div className='container mx-auto flex flex-col justify-center items-center pt-16' style={{background: `url(${grid})`,}}>
         <div>
+          <img src={direction} alt="" className='relative left-[39rem] top-8 h-[1rem] w-[2rem]' />
            <img src={hero} />
         </div>
         <div className='flex flex-col justify-center items-center px-72 text-center'>
-            <p className='font-normal leading-[28px] text-[24px] my-6'>Welcome to the</p>
-            <p className='font-bold text-[3rem] leading[56px] text-[#111111]'>Department of Computer Science Unilorin</p>
-            <p className='text-center px-32'>We provide access to technical contents such as programming, networking e.t.c</p>
-            <button className='bg-[#D8A331] text-[#fff] mt-6 rounded-[30px] px-12 h-[2.5rem]'>Learn nore</button>
+          <img src={star1} alt="" className='relative right-[36rem] bottom-44 h-[30px] w-[30px]' />
+          <img src={star2} alt="" className='relative right-36 h-[30px] w-[30px]' />
+          <img src={infinity} className='relative left-[30rem] bottom-[10rem] h-[2rem] w-[4rem]' alt="" />
+            <p className='font-normal leading-[28px] text-[24px] bottom-16 relative'>Welcome to the</p>
+            <p className='font-bold text-[3rem] leading[56px] text-[#111111] bottom-12 relative'>Department of Computer Science Unilorin</p>
+            <img src={direction} className='relative left-[26rem] bottom-24' />
+            <p className='text-center px-32 relative bottom-16'>We provide access to technical contents such as programming, networking e.t.c</p>
+            <button className='bg-[#D8A331] text-[#fff] mt-6 rounded-[30px] px-12 h-[2.5rem] relative bottom-12'>Learn nore</button>
+            <img src={infinity} alt="" className='relative right-[28rem] h-[3rem] w-[5rem] relative bottom-12' />
         </div>
     </div>
   )
