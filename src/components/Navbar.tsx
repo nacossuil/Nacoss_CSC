216cab1f57167c86f21170eb85ee67c50c30fb5d
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-08-10 22:05:16 +0100

    updated navbar and added study and repo resources

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white-800 p-4 border-[#111111] shadow-bottom-only">
+    <nav className="bg-[#fff] p-4 border-[#111111] shadow-bottom-only sticky w-full top-[0] z-[1000]">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="">
@@ -25,27 +25,37 @@ const Navbar = () => {
           } lg:flex-row space-x-4`}
         >
           <li>
-            <a href="#home" className="text-[#11111199] hover:text-[#D8A331]">
+            <a href="#home" className="text-[#11111199] hover:text-[#1D4E8D]">
               Home
             </a>
           </li>
           <li>
-            <a href="#about" className="text-[#11111199] hover:text-gray-[#D8A331]">
+            <a href="#about" className="text-[#11111199] hover:text-[#1D4E8D]">
               About
             </a>
           </li>
           <li>
-            <a href="#executives" className="text-[#11111199] hover:text-gray-[#D8A331]">
+            <a href="#executives" className="text-[#11111199] hover:text-[#1D4E8D]">
               Executives
             </a>
           </li>
           <li>
-            <a href="#events" className="text-[#11111199] hover:text-gray-[#D8A331]">
+            <a href="#events" className="text-[#11111199] hover:text-[#1D4E8D]">
               Events
             </a>
           </li>
           <li>
-            <a href="#contact" className="text-[#11111199] hover:text-gray-[#D8A331]">
+            <a href="https://drive.google.com/drive/folders/1QR9yBIMWBbQ0-lR60NhivgKJUYnjCAD1?usp=drive_link" className="text-[#11111199] hover:text-[#1D4E8D]">
+              Study Resources
+            </a>
+          </li>
+          <li>
+            <a href="https://drive.google.com/drive/folders/11teLR7hqzVXJN_Zgn9Ic5tS8GQmwvjmY?usp=drive_link" className="text-[#11111199] hover:text-[#1D4E8D]">
+              Repositories
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="text-[#11111199] hover:text-[#1D4E8D]">
               Contact
             </a>
           </li>
